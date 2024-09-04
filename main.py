8bdad5a379014bbc069740100739bc21afb6380e
Poparnassus30 <acidhack8@gmail.com>
2024-09-05 01:35:20 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,9 +5,15 @@ import datetime
 
 import sonde as s
 
+
+
 def main():
   #on creer l'objet sonde 
   sonde_moujou = s.sensor()
+  connections_serveur = soc.socket()
+  
+  while true:
+      
 
 if "__name_"==
  sys.
