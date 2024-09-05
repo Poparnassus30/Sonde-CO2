8a6f6215a0fafc22dd98899b31fef592ac6c2de9
Poparnassus30 <acidhack8@gmail.com>
2024-09-05 02:26:17 +0200

    Update Sonde.py

--- Sonde.py
+++ Sonde.py
@@ -1,6 +1,19 @@
 #!python3
 
+# ceci est une class modifier, la version initial est la NDIR.py développer par xxxxxxx (oublié lol)
+
+import smbus
+import os
+import GPIO
+
+def new_sensor(*argent,**kwarg):
+   new_sonde= sensor()
+
 class sensor():
-  def __init__():
+  __gps_point=None
+
+  def __init__(self):
     self.quantite=self.quantite+1
     
+  def sensor_conf(self,point):
+     self.__gps_point=point
